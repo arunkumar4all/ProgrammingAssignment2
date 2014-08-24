b482701f46f2f3db0c776c1e540508044c29ea4d
arunkumar4all <arunkumar@global.t-bird.edu>
2014-08-24 00:57:18 -0500

    Inverst matrix cache program
    Program to invert the matrix passed as a parameter, use cache to
    retrieve it.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# this function is to create objects that stores the
+# value of the matrix when passed on to it for the first time
+# from cache Solve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat <- NULL
+  set <- function(y) {
+    x <<- y
+    mat <<- NULL
+  }
+  get <- function() {x} 
+  setinvmat <- function(invMat) {mat <<- invMat}
+  getinvmat <- function() {mat}
+  list(set = set, get = get,
+       setinvmat = setinvmat,
+       getinvmat = getinvmat)
 }
 
+# this function returns inverse of the matrix passed on as an arguement
+# first time - create an inverse matrix object "mat" and stores the mat 
+# into setMean function created earlier (lexical scoping)
+# next time when the same object is called getintmat function returns the
+# cached object
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mat <- x$getinvmat()
+  if(!is.null(mat)) {
+    message("getting inverse matrix from cache")
+    return(mat)
+  }
+  data <- x$get()
+  mat <- solve(data)
+  x$setinvmat(mat)
+  mat
 }
